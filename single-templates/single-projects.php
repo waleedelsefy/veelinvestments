895a6b61de7b808f3dba261431c45ad6e4923cee
walee <waleedelsefy@gmail.com>
2024-11-24 00:50:48 +0200

    fix stretcher linkes

--- single-templates/single-projects.php
+++ single-templates/single-projects.php
@@ -7,28 +7,15 @@ Template Post Type: projects
 $post_id = get_the_ID();
 get_header();
 ?>
-<style>
 
-
-</style>
-<?php get_template_part('template-parts/projects/project-post-header'); ?>
+<?php get_template_part('template-parts/projects/project-gallery'); ?>
 
 <div class="project-body">
-    <div class="project-contains">
-      <?php if (has_post_thumbnail($post_id)) : ?>
-        <div class="post-thumbnail">
-          <?php
-          $featured_image_id = get_post_thumbnail_id($post_id);
-          $image_data = wp_get_attachment_image_src($featured_image_id, 'full');
-          $image_url = $image_data[0];
-          $image_width = $image_data[1];
-          $image_height = $image_data[2];
-          $image_alt = get_post_meta($featured_image_id, '_wp_attachment_image_alt', true);
-          ?>
-          <img class="post-thumbnail-img" src="<?php echo esc_url($image_url); ?>" height="<?php echo esc_attr($image_height); ?>" width="<?php echo esc_attr($image_width); ?>" alt="<?php echo esc_attr($image_alt); ?>"/>
-        </div>
-      <?php endif; ?>
+  <?php get_template_part('template-parts/projects/project-details-card'); ?>
 
+
+  <div class="flex-row">
+    <div class="col-8">
       <div class="project-details-card">
         <?php get_template_part('template-parts/projects/project-details-card'); ?>
       </div>
@@ -41,9 +28,6 @@ get_header();
       <div>
         <?php get_template_part('template-parts/projects/project-facilities'); ?>
       </div>
-      <div>
-        <?php get_template_part('template-parts/projects/project-3d-model'); ?>
-      </div>
 
       <div class="veelBlogHeaderTitle">
         <h2><?php _e('Additional Information', 'veelinvestments'); ?></h2>
@@ -54,7 +38,14 @@ get_header();
 
       <?php get_template_part('template-parts/projects/author-card'); ?>
     </div>
-    <?php get_sidebar(); ?>
+
+    <div class="sidebarAria col-4">
+      <div class="sidebar">
+        <?php get_template_part('template-parts/projects/price-container'); ?>
+        <?php get_template_part('template-parts/projects/schedule-meeting'); ?>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php
